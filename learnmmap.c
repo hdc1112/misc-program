aaea841ecdf0a86291a5b37fc96e5c2df2aa40ab
Dachuan Huang <hdc1112@gmail.com>
2012-11-21 10:47:24 -0500

    learnmmap.c updated

--- learnmmap.c
+++ learnmmap.c
@@ -1,4 +1,4 @@
-// TODO: run and learn this program
+// TODO(done): run and learn this program
 
 #include <sys/types.h>
 #include <sys/mman.h>
@@ -8,6 +8,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+
+// lesson:
+// 1. mmap can be shared between parent and child processes
 
 int main(void)
 {
@@ -20,12 +23,17 @@ int main(void)
   if ((fd = open("/dev/zero", O_RDWR, 0)) == -1)
     err(1, "open");
 
+  // hdc:
+  // create two mmap region.
   anon = mmap(NULL, 4096, PROT_READ|PROT_WRITE, MAP_ANON|MAP_SHARED, -1, 0);
   zero = mmap(NULL, 4096, PROT_READ|PROT_WRITE, MAP_FILE|MAP_SHARED, fd, 0);
 
   if (anon == MAP_FAILED || zero == MAP_FAILED)
     errx(1, "either mmap");
 
+  // hdc:
+  // the retvalue is the address of that area
+  // so, we can copy string into it
   strcpy(anon, str1);
   strcpy(zero, str1);
 
@@ -38,7 +46,7 @@ int main(void)
     case 0:
       childpid = getpid();
       printf("PID %d:\tanonymous %s, zero-backed %s\n", childpid, anon, zero);
-      sleep(3);
+      sleep(5);
 
       printf("PID %d:\tanonymous %s, zero-backed %s\n", childpid, anon, zero);
       munmap(zero, 4096);
