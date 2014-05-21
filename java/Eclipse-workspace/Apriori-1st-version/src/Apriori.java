3cc4173152d6cc6c4a18a64827645b13db8cabbf
Dachuan Huang <hdc1112@gmail.com>
2014-05-21 10:48:28 -0700

    some comments added in Apriori-1st-version

--- java/Eclipse-workspace/Apriori-1st-version/src/Apriori.java
+++ java/Eclipse-workspace/Apriori-1st-version/src/Apriori.java
@@ -21,6 +21,7 @@ import java.util.*;
 public class Apriori {
 
   public static void main(String[] args) {
+    // hdc: program starts at here
     AprioriCalculation ap = new AprioriCalculation();
 
     ap.aprioriProcess();
@@ -32,6 +33,12 @@ public class Apriori {
  *****************************************************************************/
 class AprioriCalculation
 {
+  // hdc: remember that in Apriori algorithm, candidate set,
+  // hdc: strictly speaking, is a set of set.
+  // hdc: and here, "String" is the "set", and Vector<String> is the "set of set"
+  // hdc: the "set" format is 345, this means, the item 2 3 4 are inside this set
+  // hdc: this implementation didn't say explicity about Ci and Li
+  // hdc: they are all stored inside ths candidates data structure
   Vector<String> candidates=new Vector<String>(); //the current candidates
   String configFile="config.txt"; //configuration file
   String transaFile="transa.txt"; //transaction file
@@ -54,6 +61,7 @@ class AprioriCalculation
     long start, end; //start and end time
     int itemsetNumber=0; //the current itemset being looked at
     //get config
+    // hdc: read #transactions, #items, minimum support
     getConfig();
 
     System.out.println("Apriori algorithm has started.\n");
@@ -69,16 +77,27 @@ class AprioriCalculation
       itemsetNumber++;
 
       //generate the candidates
+      // hdc: generate candidate sets based on previous round's candidate set
+      // and the current itemsetNumber. there's no pruning in this implementation.
       generateCandidates(itemsetNumber);
+      // hdc: now we have the Ci
 
       //determine and display frequent itemsets
+      // hdc: for each candidate itemset, decide whether its appearance
+      // has exceeded minimum support.
       calculateFrequentItemsets(itemsetNumber);
+
+      // hdc: now we have the Li
+
       if(candidates.size()!=0)
       {
+        // hdc: at this level, the candidates set print (Li)
+        // hdc: this if stmt is only for printing purpose
         System.out.println("Frequent " + itemsetNumber + "-itemsets");
         System.out.println(candidates);
       }
       //if there are <=1 frequent items, then its the end. This prevents reading through the database again. When there is only one frequent itemset.
+      // hdc: if Li's size is 1 or 0, then Ci+1 must be empty, so no need to proceed
     }while(candidates.size()>1);
 
     //end timer
@@ -182,6 +201,8 @@ class AprioriCalculation
       System.out.println();
       minSup/=100.0;
 
+      // hdc: I don't know what oneval is used for
+      // hdc: but by default its element are all "1"
       oneVal = new String[numItems];
       System.out.print("Enter 'y' to change the value each row recognizes as a '1':");
       if(getInput().compareToIgnoreCase("y")==0)
@@ -220,6 +241,7 @@ class AprioriCalculation
    *************************************************************************/
   private void generateCandidates(int n)
   {
+    // hdc: this function is used to calculate Ci+1 from Li
     Vector<String> tempCandidates = new Vector<String>(); //temporary candidate string vector
     String str1, str2; //strings that will be used for comparisons
     StringTokenizer st1, st2; //string tokenizers for the two itemsets being compared
@@ -291,6 +313,10 @@ class AprioriCalculation
    *************************************************************************/
   private void calculateFrequentItemsets(int n)
   {
+    // hdc: the input to this function is
+    // hdc: n, the iteration counter
+    // hdc: candidates, or Cn, frequentCandidates will be a subset of Cn
+    // hdc: and frequentCandidates will be assigned to candidates
     Vector<String> frequentCandidates = new Vector<String>(); //the frequent candidates for the current itemset
     FileInputStream file_in; //file input stream
     BufferedReader data_in; //data input stream
@@ -299,7 +325,10 @@ class AprioriCalculation
 
     StringTokenizer st, stFile; //tokenizer for candidate and transaction
     boolean match; //whether the transaction has all the items in an itemset
+    // hdc: trans[] array stores the row-wise item information
     boolean trans[] = new boolean[numItems]; //array to hold a transaction so that can be checked
+    // hdc: this records every "set" 's occurrences
+    // hdc: count[] is used to filter candidates
     int count[] = new int[candidates.size()]; //the number of successful matches
 
     try
@@ -312,6 +341,13 @@ class AprioriCalculation
       data_in = new BufferedReader(new InputStreamReader(file_in));
 
       //for each transaction
+      // hdc: i.e., for each row
+      // hdc: the whole logic of the following code is:
+      // hdc: for each row
+      // hdc:     for each candidate in candidates
+      // hdc:         if this candidate appears in this row
+      // hdc:             count[this candidate]++
+      // hdc: now filter the candidates
       for(int i=0; i<numTransactions; i++)
       {
         //System.out.println("Got here " + i + " times"); //useful to debug files that you are unsure of the number of line
@@ -319,19 +355,29 @@ class AprioriCalculation
         //put the contents of that line into the transaction array
         for(int j=0; j<numItems; j++)
         {
+          // hdc: oneVal[] array is used to tell that in this row
+          // hdc: whether 0 should be treated as true, or 1 should be treated as true
+          // hdc: if it matches oneVal[i], then compareToIgnoreCase() would return 0
+          // hdc: and the whole right expression would return true
           trans[j]=(stFile.nextToken().compareToIgnoreCase(oneVal[j])==0); //if it is not a 0, assign the value to true
         }
 
+        // hdc: now we have a trans[] array
+
         //check each candidate
+        // hdc: i.e., check each "set" in the "set of set"
         for(int c=0; c<candidates.size(); c++)
         {
           match = false; //reset match to false
           //tokenize the candidate so that we know what items need to be present for a match
+          // hdc: for each set in the candidates set, check its existence
           st = new StringTokenizer(candidates.get(c));
+          // hdc: now we have a set, check whether this set belongs to this row's transaction
           //check each item in the itemset to see if it is present in the transaction
           while(st.hasMoreTokens())
           {
             match = (trans[Integer.valueOf(st.nextToken())-1]);
+            // hdc: if one item doesn't show up in this row, then it's not a match
             if(!match) //if it is not present in the transaction stop checking
               break;
           }
@@ -344,6 +390,7 @@ class AprioriCalculation
       {
         //  System.out.println("Candidate: " + candidates.get(c) + " with count: " + count + " % is: " + (count/(double)numItems));
         //if the count% is larger than the minSup%, add to the candidate to the frequent candidates
+        // hdc: filter each candidate out
         if((count[i]/(double)numTransactions)>=minSup)
         {
           frequentCandidates.add(candidates.get(i));
