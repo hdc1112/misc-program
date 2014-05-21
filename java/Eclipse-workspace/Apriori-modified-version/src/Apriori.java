884d93002d9758dcd8f858f3b89045531e9c8d42
Dachuan Huang <hdc1112@gmail.com>
2014-05-21 15:14:07 -0700

    Apriori-modified-version added, prepared for reimpl

---
+++ java/Eclipse-workspace/Apriori-modified-version/src/Apriori.java
@@ -0,0 +1,17 @@
+
+public class Apriori {
+	
+	public void config() {
+		
+	}
+	
+	public void start() {
+		
+	}
+
+	public static void main(String[] args) {
+		Apriori apriori = new Apriori();
+		apriori.config();
+		apriori.start();
+	}
+}
