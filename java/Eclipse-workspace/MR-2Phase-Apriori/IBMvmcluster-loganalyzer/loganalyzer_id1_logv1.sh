2f940be9f69441340932a5fbe23395941deb2a56
Dachuan Huang <hdc1112@gmail.com>
2014-06-21 10:16:51 -0700

    loganalyzer_id1_logv1.sh updated

--- java/Eclipse-workspace/MR-2Phase-Apriori/IBMvmcluster-loganalyzer/loganalyzer_id1_logv1.sh
+++ java/Eclipse-workspace/MR-2Phase-Apriori/IBMvmcluster-loganalyzer/loganalyzer_id1_logv1.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# lesson for future
+# inside function, use "local" keyword to declare variable
+
 # standard header
 set -x
 absme=`readlink -f $0`
@@ -7,8 +10,21 @@ abshere=`dirname $absme`
 me=`basename $absme`
 cd $abshere
 
-# output format of this script is in 
+# output, and output format of this script is in 
 # 6.19.2014.design.exp.chess.dat.header.file.xlsx
+
+# interactive input
+# TODO read the backup log folder's name from user
+# TODO read the stub program's log path from user
+# TODO read the output file path from user
+#read -p "Server backup log folder path: " user_servbakupf
+read -p "Statistics output path: " user_statpath
+if [ -z $user_statpath ]; then
+  echo Invalid Statistics output path
+  exit 2
+fi
+rm -f $user_statpath
+touch $user_statpath
 
 # input of this script
 # hardware configuration
@@ -31,20 +47,229 @@ for n in $in_sshnodes; do
 done
 # this script assume log is in stderr in the following folder
 in_userlogs=userlogs
+# log version
+logv="logv 1"
+prefix="\[MR2PhaseApriori\]\[$logv\]\ "
 # this script assume that the remote logs are organized in:
 # logs-hadoop1 --> userlogs --> application_*_0001 --> container_*_0001 --> stderr,stdout
 # this script assume that every log folder has exactly the same structure
 # and no corrupted message
+# this script has many implicit assumptions, such as #mappers are 2,
+# #reducers are 1, no speculative, etc.
+# Just remember, whenever there is some change, no matter what change,
+# the first thought should be whether this script would still work.
 
 # debug param
 deb_clean=yes
 
 # under this line, there's no hard-coded thing
 
+# functions
+function get_m1_1_start {
+  l_totalcont=$1 && shift
+  l_interestfiles=($@)
+  # TODO change all the following default value to "NULL", or change in the other place, use "if empty then NULL" syntax
+  m1_1_start=""
+  for i in `seq 0 $((l_totalcont-1))`; do
+    #echo ${l_interestfiles[$i]}
+    set +x
+    while read line; do
+      [[ $line =~ $prefix\(1/2\)\ 0\ Map\ [tT]ask\ start\ time:\ ([0-9]*) ]]\
+        && m1_1_start=${BASH_REMATCH[1]} && break
+    done < <(cat ${l_interestfiles[$i]}/stderr)
+    set -x
+    if [ ! -z $m1_1_start ]; then
+      break
+    fi
+  done
+  echo $m1_1_start
+}
+
+function get_m1_1_end {
+  l_totalcont=$1 && shift
+  l_interestfiles=($@)
+  m1_1_end=""
+  for i in `seq 0 $((l_totalcont-1))`; do
+    #echo ${l_interestfiles[$i]}
+    set +x
+    while read line; do
+      [[ $line =~ $prefix\(1/2\)\ 0\ Map\ [tT]ask\ end\ time:\ ([0-9]*) ]]\
+        && m1_1_end=${BASH_REMATCH[1]} && break
+    done < <(cat ${l_interestfiles[$i]}/stderr)
+    set -x
+    if [ ! -z $m1_1_end ]; then
+      break
+    fi
+  done
+  echo $m1_1_end
+}
+
+function get_m1_1_loop {
+  l_totalcont=$1 && shift
+  l_interestfiles=($@)
+  m1_1_loop=""
+  for i in `seq 0 $((l_totalcont-1))`; do
+    #echo ${l_interestfiles[$i]}
+    set +x
+    while read line; do
+      [[ $line =~ $prefix\(1/2\)\ 0\ Map\ [tT]ask\ end\ time:\ ([0-9]*) ]]\
+        && m1_1_loop=${BASH_REMATCH[1]} && break
+    done < <(cat ${l_interestfiles[$i]}/stderr)
+    set -x
+    if [ ! -z $m1_1_loop ]; then
+      break
+    fi
+  done
+  echo $m1_1_loop
+}
+
+function get_m1_1 {
+  l_totalcont=$1 && shift
+  l_interestfiles=($@)
+  m1_1=""
+  for i in `seq 0 $((l_totalcont-1))`; do
+    #echo ${l_interestfiles[$i]}
+    set +x
+    while read line; do
+      [[ $line =~ $prefix\(1/2\)\ 0\ Map\ [tT]ask\ execution\ time:\ ([0-9]*) ]]\
+        && m1_1=${BASH_REMATCH[1]} && break
+    done < <(cat ${l_interestfiles[$i]}/stderr)
+    set -x
+    if [ ! -z $m1_1 ]; then
+      break
+    fi
+  done
+  echo $m1_1
+}
+
+function get_m1_2_start {
+  l_totalcont=$1 && shift
+  l_interestfiles=($@)
+  m1_2_start=""
+  for i in `seq 0 $((l_totalcont-1))`; do
+    #echo ${l_interestfiles[$i]}
+    set +x
+    while read line; do
+      [[ $line =~ $prefix\(1/2\)\ 1\ Map\ [tT]ask\ start\ time:\ ([0-9]*) ]]\
+        && m1_2_start=${BASH_REMATCH[1]} && break
+    done < <(cat ${l_interestfiles[$i]}/stderr)
+    set -x
+    if [ ! -z $m1_2_start ]; then
+      break
+    fi
+  done
+  echo $m1_2_start
+}
+
+function get_m1_2_end {
+  l_totalcont=$1 && shift
+  l_interestfiles=($@)
+  m1_2_end=""
+  for i in `seq 0 $((l_totalcont-1))`; do
+    #echo ${l_interestfiles[$i]}
+    set +x
+    while read line; do
+      [[ $line =~ $prefix\(1/2\)\ 1\ Map\ [tT]ask\ end\ time:\ ([0-9]*) ]]\
+        && m1_2_end=${BASH_REMATCH[1]} && break
+    done < <(cat ${l_interestfiles[$i]}/stderr)
+    set -x
+    if [ ! -z $m1_2_end ]; then
+      break
+    fi
+  done
+  echo $m1_2_end
+}
+
+function get_m1_2 {
+  l_totalcont=$1 && shift
+  l_interestfiles=($@)
+  m1_2=""
+  for i in `seq 0 $((l_totalcont-1))`; do
+    #echo ${l_interestfiles[$i]}
+    set +x
+    while read line; do
+      [[ $line =~ $prefix\(1/2\)\ 1\ Map\ [tT]ask\ execution\ time:\ ([0-9]*) ]]\
+        && m1_2=${BASH_REMATCH[1]} && break
+    done < <(cat ${l_interestfiles[$i]}/stderr)
+    set -x
+    if [ ! -z $m1_2 ]; then
+      break
+    fi
+  done
+  echo $m1_2
+}
+
+function get_r1_1_start {
+  l_totalcont=$1 && shift
+  l_interestfiles=($@)
+  r1_1_start=""
+  for i in `seq 0 $((l_totalcont-1))`; do
+    #echo ${l_interestfiles[$i]}
+    set +x
+    while read line; do
+      [[ $line =~ $prefix\(1/2\)\ 0\ Reduce\ [tT]ask\ start\ time:\ ([0-9]*) ]]\
+        && r1_1_start=${BASH_REMATCH[1]} && break
+    done < <(cat ${l_interestfiles[$i]}/stderr)
+    set -x
+    if [ ! -z $r1_1_start ]; then
+      break
+    fi
+  done
+  echo $r1_1_start
+}
+
+function get_r1_1_end {
+  l_totalcont=$1 && shift
+  l_interestfiles=($@)
+  r1_1_end=""
+  for i in `seq 0 $((l_totalcont-1))`; do
+    #echo ${l_interestfiles[$i]}
+    set +x
+    while read line; do
+      [[ $line =~ $prefix\(1/2\)\ 0\ Reduce\ [tT]ask\ end\ time:\ ([0-9]*) ]]\
+        && r1_1_end=${BASH_REMATCH[1]} && break
+    done < <(cat ${l_interestfiles[$i]}/stderr)
+    set -x
+    if [ ! -z $r1_1_end ]; then
+      break
+    fi
+  done
+  echo $r1_1_end
+}
+
+function get_r1_1 {
+  l_totalcont=$1 && shift
+  l_interestfiles=($@)
+  r1_1=""
+  for i in `seq 0 $((l_totalcont-1))`; do
+    #echo ${l_interestfiles[$i]}
+    set +x
+    while read line; do
+      [[ $line =~ $prefix\(1/2\)\ 0\ Reduce\ execution\ time:\ ([0-9]*) ]]\
+        && r1_1=${BASH_REMATCH[1]} && break
+    done < <(cat ${l_interestfiles[$i]}/stderr)
+    set -x
+    if [ ! -z $r1_1]; then
+      break
+    fi
+  done
+  echo $r1_1
+}
+
+function clean {
+  l_cleanf=$1
+  if [ $l_cleanf = "yes" ]; then
+    if [ ! -z $in_localrepo ]; then
+      rm -r -f $in_localrepo
+    fi
+  fi
+}
+
 # prepare for main logic
 for n in $in_nodes; do
   scp -r -q $in_user@$n:$in_remotehadooplogs $in_localrepo/logs-$n
 done
+# TODO try to back up the log folder into hadoop-logs in server node
 
 # main logic
 mid_maxjobid=0
@@ -69,11 +294,18 @@ else
   mid_maxjobid=$((mid_maxjobid-1))
 fi
 
+if [ $mid_maxjobid -lt 0 ]; then
+  echo Exit, sample space too small
+  clean $deb_clean
+  exit 1
+fi
+
 echo mid_maxjobid=$mid_maxjobid
 mid_totalrun=$((mid_maxjobid/2))
 echo mid_totalrun=$mid_totalrun
 
 for run in `seq 1 $mid_totalrun`; do
+  # for phase 1
   runid=$((run*2-1))
   run1stfolder=
 
@@ -99,17 +331,71 @@ for run in `seq 1 $mid_totalrun`; do
 
   done
 
-  for j in `seq 1 $totalcont`; do
-    echo haha ${interestfiles[$j]}
-  done
+  # now i have phase 1's each task's container stderr
+  #for j in `seq 1 $totalcont`; do
+  #echo ${interestfiles[$j]}
+  #done
+
+  m1_1_start=$(get_m1_1_start $totalcont "${interestfiles[@]}")
+  echo -n $m1_1_start" " >> $user_statpath
+
+  m1_1_end=$(get_m1_1_end $totalcont "${interestfiles[@]}")
+  echo -n $m1_1_end" " >> $user_statpath
+
+  # TODO in program, add map task id into log, and update log version
+  #m1_1_loop=$(get_m1_1_loop $totalcont "${interestfiles[@]}")
+  #echo -n $m1_1_loop" "
+
+  m1_1=$(get_m1_1 $totalcont "${interestfiles[@]}")
+  echo -n $m1_1" " >> $user_statpath
+
+  m1_2_start=$(get_m1_2_start $totalcont "${interestfiles[@]}")
+  echo -n $m1_2_start" " >> $user_statpath
+
+  m1_2_end=$(get_m1_2_end $totalcont "${interestfiles[@]}")
+  echo -n $m1_2_end" " >> $user_statpath
+
+  #m1_2_loop=$(get_m1_2_loop $totalcont "${interestfiles[@]}")
+  #echo -n $m1_2_loop" "
+
+  m1_2=$(get_m1_2 $totalcont "${interestfiles[@]}")
+  echo -n $m1_2" " >> $user_statpath
+
+  r1_1_start=$(get_r1_1_start $totalcont "${interestfiles[@]}")
+  echo -n $r1_1_start" " >> $user_statpath
+
+  r1_1_end=$(get_r1_1_end $totalcont "${interestfiles[@]}")
+  echo -n $r1_1_end" " >> $user_statpath
+
+  r1_1=$(get_r1_1 $totalcont "${interestfiles[@]}")
+  echo -n $r1_1" " >> $user_statpath
+
+
+  # for phase 2
+  # TODO keep developing these code
+  # m2_1_start
+  # m2_1_end
+  # m2_1
+  # m2_1_cache_total
+  # m2_1_cache_hit
+  # m2_2_start
+  # m2_2_end
+  # m2_2
+  # m2_2_cache_total
+  # m2_2_cache_hit
+  # r2_1_start
+  # r2_1_end
+  # r2_1
+
+  echo >> $user_statpath
 
 done
 
-
 # clean
-if [ $deb_clean == "yes" ]; then
-  rm -r -f $in_localrepo
-fi
+#if [ $deb_clean == "yes" ]; then
+#  rm -r -f $in_localrepo
+#fi
+clean $deb_clean
 
 # standard tail
 set +x
